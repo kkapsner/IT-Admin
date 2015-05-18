51bb811d952d1fd9384641ae748752c3710ecfa3
kkapsner <github@mail.kkapsner.de>
2015-05-18 12:54:23 +0200

    Added named computer query

--- actions/show.php
+++ actions/show.php
@@ -1,6 +1,15 @@
 <?php
 
-if (array_key_exists("id", $_GET) && $_GET["id"] != 0){
+if ($class === "Computer" && array_key_exists("name", $_GET)){
+	$items = DBItem::getByConditionCLASS($class, "`name` LIKE " . $db->quote("%" . $_GET["name"], DB::PARAM_STR));
+	if (count($items) === 1){
+		$temp->content .= $items[0]->view(false, false);
+	}
+	else {
+		$temp->content .= $items->view(false, false);
+	}
+}
+elseif (array_key_exists("id", $_GET) && $_GET["id"] != 0){
 	$item = DBItem::getCLASS($class, $_GET["id"]);
 	$temp->content .= $item->view(false, false);
 }
